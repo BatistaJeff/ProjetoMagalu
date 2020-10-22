80204356e77974e1aa6b5bf4bbe472d0d92a0f82
Jefferson Batista Magalhaes <trabalhojbm@gmail.com>
2020-10-22 03:52:28 -0300

    Commit de inicio da construção da estrutura do projeto

---
+++ src/main/java/br/com/ProjetoMagalu/ProjetoMagaluApplication.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoMagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ProjetoMagalu/ProjetoMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoMagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
